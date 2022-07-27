55fa2fb46b6ccef7fbaf0ee055319adcd765f7d3
miss <ddc.akari@gmail.com>
2022-07-27 20:59:44 +0900

    save

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./routes/index.js";
+import store from "./store/index.js";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(store).mount("#app");
 
 // use() : 플러그인 연결할 때 사용하는 메소드
