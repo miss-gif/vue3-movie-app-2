f156b4d144708032c1a44a034470cd1a099071b1
miss <ddc.akari@gmail.com>
2022-07-30 07:00:49 +0900

    Vuex 구성

--- src/main.js
+++ src/main.js
@@ -4,6 +4,5 @@ import App from "./App.vue";
 import router from "./routes/index.js";
 import store from "./store/index.js";
 
-createApp(App).use(router).use(store).mount("#app");
-
 // use() : 플러그인 연결할 때 사용하는 메소드
+createApp(App).use(router).use(store).mount("#app");
