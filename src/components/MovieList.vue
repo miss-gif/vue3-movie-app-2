bddcf824274fa63901f2b8444dd7a9a25321a470
miss <ddc.akari@gmail.com>
2022-07-30 07:44:22 +0900

    영화 검색 기능

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -3,7 +3,8 @@
     <div class="inner">
       <MovieItem
         v-for="movie in movies"
-        :key="movie.imdbID" />
+        :key="movie.imdbID"
+        :movie="movie" />
     </div>
   </div>
 </template>
@@ -14,6 +15,11 @@ import MovieItem from '~/components/MovieItem'
 export default {
   components: {
     MovieItem
+  },
+  computed: {
+    movies() {
+      return this.$store.state.movie.movies
+    }
   }
 }
 </script>
--- src/store/movie.js
+++ src/store/movie.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export default {
   // module로 사용
   namespaced: "true",
@@ -6,20 +8,32 @@ export default {
     movies: [],
   }),
   // computed
-  getters: {
-    movieIds(state) {
-      return state.movies.map((m) => m.imdbID);
-    },
-  },
+  getters: {},
   // methods
   // 변이 (데이터 수정 가능)
   mutations: {
+    updateState(state, payload) {
+      Object.keys(payload).forEach((key) => {
+        state[key] = payload[key];
+      });
+    },
     resetMovies(state) {
       state.movies = [];
     },
   },
   // 비동기
   actions: {
-    searchMovies() {},
+    async searchMovies({ commit }, payload) {
+      const { title, type, number, year } = payload;
+      const OMDB_API_KEY = "7035c60c";
+
+      const res = await axios.get(
+        `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`
+      );
+      const { Search, totalResults } = res.data;
+      commit("updateState", {
+        movies: Search,
+      });
+    },
   },
 };
