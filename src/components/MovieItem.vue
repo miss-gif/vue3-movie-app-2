ec43ec3f7d4b87fefb0642acbb3b31bcbf3a20cd
miss <ddc.akari@gmail.com>
2022-08-10 00:11:28 +0900

    영화 아이템 - 기본 출력

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="movie">
-    <img
-      :src="movie.Poster"
-      alt=""
-      width="200" />
+  <div 
+    :style="{backgroundImage: `url(${movie.Poster})`}"
+    class="movie">
+    <div class="info">
+      <div class="year">
+        {{ movie.Year }}
+      </div>
+      <div class="title">
+        {{ movie.Title }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,6 +24,27 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import "~/scss/main.scss";
+.movie{
+  $width:200px;
+  width: $width;
+  height: $width*3/2;
+  margin: 10px;
+  border-radius: 4px;
+  background-color: $gray-400;
+  background-size: cover;
+  overflow: hidden;
+  position: relative;
+  .info{
+    background-color: rgba($black, .3);
+    width: 100%;
+    padding: 14px;
+    font-size: 14px;
+    text-align: center;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+  }
+}
 </style>
