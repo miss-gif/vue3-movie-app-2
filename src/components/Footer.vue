947827925fb46f5886f39b3c539481d534688007
miss <ddc.akari@gmail.com>
2022-08-14 22:20:03 +0900

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer>
+    <Logo />
+    <a
+      href="https://www.naver.com/"
+      target="_blank">
+      (c){{ new Date().getFullYear() }} test
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from "~/components/Logo";
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  footer {
+    padding: 70px 0;
+    text-align: center;
+    opacity: .7;
+    .logo {
+      display: block;
+      margin-bottom: 4px;
+    }
+  }
+</style>
