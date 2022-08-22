ef3609415d4024f788f2fb51b47682f779131115
miss <ddc.akari@gmail.com>
2022-08-22 17:49:30 +0900

    save

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1>Moviesdfmgkl</h1>
+  <div class="container">
+    <div class="skeletons">
+      <div class="skeleton poster">
+        <div class="specs">
+          <div class="skeleton title"></div>
+          <div class="skeleton spec"></div>
+          <div class="skeleton plot"></div>
+          <div class="skeleton etc"></div>
+          <div class="skeleton etc"></div>
+          <div class="skeleton etc"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,6 +26,41 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "~/scss/main";
 
+.container {
+  padding-top: 40px;
+}
+.skeletons {
+  display: flex;
+  .poster {
+    width: 500px;
+    height: 500px * 3/2;
+    margin-right: 70px;
+  }
+  .specs {
+
+  }
+  .skeleton{
+    border-radius: 10px;
+    background-color: $gray-200;
+    &.title {
+      width: 80%;
+      height: 70px;
+    }
+    &.spec {
+      width: 60%;
+      height: 30px;
+    }
+    &.plot {
+      width: 100%;
+      height: 250px;
+    }
+    &.etc {
+      width: 50%;
+      height: 50px;
+    }
+  }
+}
 </style>
