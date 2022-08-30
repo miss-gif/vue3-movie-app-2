48dbddb165d2e601a14521071b42ce4057f8166b
miss <ddc.akari@gmail.com>
2022-08-30 20:20:25 +0900

    save

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -2,6 +2,10 @@
   <div 
     :style="{backgroundImage: `url(${movie.Poster})`}"
     class="movie">
+    <Loader 
+      v-if="imageLoading"
+      :size="1.5"
+      absolute />
     <div class="info">
       <div class="year">
         {{ movie.Year }}
@@ -14,13 +18,35 @@
 </template>
 
 <script>
+import Loader from '~/components/Loader';
+
 export default {
+  component: {
+    Loader
+  },
   props: {
     movie: {
       type: Object,
       default: ()=> ({})
     }
-  }
+  },
+  data() {
+    return {
+      imageLoading: true
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      const img = document.createElement('img')
+      img.src = this.movie.Poster
+      img.addEventListener('load', ()=> {
+        this.imageLoading = false
+      })
+    }
+  },
 }
 </script>
 
