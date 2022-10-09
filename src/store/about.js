acc0691f2a8af2edd21d0e60c2c469a018e1e1bf
miss <ddc.akari@gmail.com>
2022-10-10 06:59:18 +0900

    fix

--- src/store/about.js
+++ src/store/about.js
@@ -5,6 +5,7 @@ export default {
     email: "email",
     blog: "blog",
     phone: "phone",
-    image: "",
+    image:
+      "https://lh3.googleusercontent.com/cS5nvr3r6Q16NoV6IuJLaauz7HNNRPnuHtsHleZ8du594H4EeiOjeNxV-Nq_w-qRA87TUedLQjTmqCG5s6jNZRp29n571FDWyditF-WJhfhQTY_73OM",
   }),
 };
